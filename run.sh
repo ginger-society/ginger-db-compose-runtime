f53829bc245aebabe3d6abb28d4861a744b60a00
Ginger Society Admin <admin@gingersociety.org>
2026-04-24 14:10:14 +0530

    chore: minor

--- run.sh
+++ run.sh
@@ -9,6 +9,6 @@ export DJANGO_SETTINGS_MODULE=server.settings
 gunicorn server.wsgi:application \
   --bind 0.0.0.0:8000 \
   --workers 1 \
-  --threads 2 \
+  --threads 1 \
   --worker-class sync \
   --timeout 60
